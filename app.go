8f73fd6731eb0196bb57a0c91d648f7ce04c7eec
IndominusByte <nyomanpradipta120@gmail.com>
2021-10-20 11:56:03 +0800

    init

---
+++ app.go
@@ -0,0 +1,7 @@
+package main
+
+import gosayhello "github.com/IndominusByte/go-say-hello"
+
+func main() {
+	gosayhello.SayHello("oman")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module example
+
+go 1.17
+
+require github.com/IndominusByte/go-say-hello v0.1.1
